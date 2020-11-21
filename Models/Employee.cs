a3447b3399e4d455db181cdf506f22a2ec9edc55
Wayne A. Tipton <wayne.tipton@gmail.com>
2020-11-21 11:39:29 -0600

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManager.Mvc.Models
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        [Column("EmployeeID")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required(ErrorMessage = "Employee ID is required")]
+        [Display(Name = "Employee ID")]
+        public int EmployeeID { get; set; }
+
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(10,ErrorMessage = "First Name must be less than 10 characters")]
+        public string FirstName { get; set; }
+
+        [Column("LastName")]
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(20,ErrorMessage = "Last Name must be less than 20 characters")]
+        public string LastName { get; set; }
+    }
+}
+.
